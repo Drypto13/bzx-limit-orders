6a05bffd52d9b466033f7974f5023d38fcaada88
bZxStats <51417606+bZxStats@users.noreply.github.com>
2021-07-16 14:13:59 -0700

    Update init_deploy.py

--- scripts/init_deploy.py
+++ scripts/init_deploy.py
@@ -1,13 +1,13 @@
 from brownie import *
 
 def main():
-    accounts.load('main1')
+    accounts.load('main3')
     factory = deploy_base_contracts() 
     print(factory.address)
 def deploy_base_contracts():
     factoryContract = walletFactor.deploy({'from':accounts[0]}) #deploy factory
     smartW = SmartWallet.deploy({'from':accounts[0]})
-    proxyFactory = FactoryContractProxy.deploy({'from':accounts[0]})
+    proxyFactory = FactoryContractProxy.deploy("0xfe4F0eb0A1Ad109185c9AaDE64C48ff8e928e54B",{'from':accounts[0]})
     proxyFactory.setImpl(factoryContract.address,{'from':accounts[0]})
     walletCreation = walletCreator.deploy({'from':accounts[0]}) #deploy wallet generator
     walletCreation.setFact(proxyFactory.address,{'from':accounts[0]}) #assign the factory address to the generator
@@ -15,5 +15,6 @@ def deploy_base_contracts():
     proxyContract.setWalletLogic(smartW.address,{'from':accounts[0]})
     proxyContract.setGenerator(walletCreation.address,{'from':accounts[0]}) #assign generatory address to factory
     return proxyContract
+
     
     
