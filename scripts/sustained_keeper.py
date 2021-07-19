0cb144f03a64820fb90c7768230bb3f4a663e3b4
bZxStats <51417606+bZxStats@users.noreply.github.com>
2021-07-19 11:55:47 -0700

    Add files via upload

--- scripts/sustained_keeper.py
+++ scripts/sustained_keeper.py
@@ -1,17 +1,18 @@
 from brownie import *
 import time
 def main():
-    accounts.load('main1')
+    accounts.load('main3')
     loop_check()
 
 def loop_check():
-    factoryAddress = ''
-    factoryContract = interface.IFactory(factoryAddress)
+    factoryAddress = '0x9952f51F072182172bbEFe38382D45a4E2bb4bEB'
+    factoryContract = Contract.from_abi("walletFactor",factoryAddress,walletFactor.abi)
     totalTraders = factoryContract.getTotalTradersWithOrders.call()
     getTraderAddresses = factoryContract.getTradersWithOrders.call(0,totalTraders)
     for x in getTraderAddresses:
         totalActiveTrades = factoryContract.getTotalOrders.call(x)
         for y in factoryContract.getActiveOrderIDs.call(x,0,totalActiveTrades):
+            print(factoryContract.getOrderByOrderID(x,y))
             if factoryContract.checkIfExecutable.call(x,y):
                 factoryContract.executeOrder(x,y,{'from':accounts[0]})
             else:
