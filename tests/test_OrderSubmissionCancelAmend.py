37e32bea03c8d06accfadd73aad73cd73d65f41c
bZxStats <51417606+bZxStats@users.noreply.github.com>
2021-07-20 21:03:15 -0700

    Update test_OrderSubmissionCancelAmend.py

--- tests/test_OrderSubmissionCancelAmend.py
+++ tests/test_OrderSubmissionCancelAmend.py
@@ -9,7 +9,7 @@ def shared_setup(module_isolation):
 def deploy_limit_contracts(bzx,accounts,walletFactor,SmartWallet,FactoryContractProxy,walletCreator):
     factoryContract = walletFactor.deploy(bzx.address,{'from':accounts[0]}) #deploy factory
     smartW = SmartWallet.deploy({'from':accounts[0]})
-    proxyFactory = FactoryContractProxy.deploy({'from':accounts[0]})
+    proxyFactory = FactoryContractProxy.deploy(bzx.address,{'from':accounts[0]})
     proxyFactory.setImpl(factoryContract.address,{'from':accounts[0]})
     walletCreation = walletCreator.deploy({'from':accounts[0]}) #deploy wallet generator
     walletCreation.setFact(proxyFactory.address,{'from':accounts[0]}) #assign the factory address to the generator
