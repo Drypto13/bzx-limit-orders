83a36b6b592b88718ae51d37e44f49396b2d2557
bZxStats <51417606+bZxStats@users.noreply.github.com>
2021-07-22 13:53:34 -0700

    Add files via upload

--- contracts/SmartWallet.sol
+++ contracts/SmartWallet.sol
@@ -20,13 +20,12 @@ contract SmartWallet is MainWalletEvents,SmartWalletStorage{
     function getBZXRouter() public view returns (address){
         return IWalletFactory(factoryContract).getRouter();
     } 
-    function executeTradeFactoryOpen(address payable keeper, address iToken, uint loanTokenAmount, address collateralAddress, uint collateralAmount, uint leverage, bytes32 lid,uint feeAmount) onlyFactory() public returns(bool success){
-        bytes memory arbData = "";
+    function executeTradeFactoryOpen(address payable keeper, address iToken, uint loanTokenAmount, address collateralAddress, uint collateralAmount, uint leverage, bytes32 lid,uint feeAmount,bytes memory arbData) onlyFactory() public returns(bool success){
 		LoanTokenI(iToken).marginTrade(lid,leverage,loanTokenAmount,collateralAmount,collateralAddress,address(this),arbData);
         _safeTransfer(collateralAmount > loanTokenAmount ? collateralAddress : LoanTokenI(iToken).loanTokenAddress(),keeper,feeAmount,"");     
         success = true;
     }
-    function executeTradeFactoryClose(address payable keeper, bytes32 loanID, uint amount, bool iscollateral,address loanTokenAddress, address collateralAddress,uint feeAmount) onlyFactory() public returns(bool success){
+    function executeTradeFactoryClose(address payable keeper, bytes32 loanID, uint amount, bool iscollateral,address loanTokenAddress, address collateralAddress,uint feeAmount,bytes memory arbData) onlyFactory() public returns(bool success){
         bytes memory arbData = "";
         IBZx(getBZXRouter()).closeWithSwap(loanID, address(this), amount, iscollateral, arbData);
 		if((iscollateral == true && collateralAddress != BNBAddress) || (iscollateral == false && loanTokenAddress != BNBAddress)){
@@ -46,13 +45,11 @@ contract SmartWallet is MainWalletEvents,SmartWalletStorage{
     function cancelOrder(uint nonce) onlyOwner() public{
         IWalletFactory(factoryContract).cancelOrder(nonce);
     }
-    function openPosition(bytes32 loanId, address iToken, uint loanTokenAmount, address collateralAddress, uint collateralAmount, uint leverage) onlyOwner() public returns(bool success){
-        bytes memory arbData = "";
+    function openPosition(bytes32 loanId, address iToken, uint loanTokenAmount, address collateralAddress, uint collateralAmount, uint leverage, bytes memory arbData) onlyOwner() public returns(bool success){
         LoanTokenI(iToken).marginTrade(loanId,leverage,loanTokenAmount,collateralAmount,collateralAddress,address(this),arbData);
         success = true;
     }
-    function closePosition(bytes32 loanId,uint amount, bool iscollateral)  public{
-        bytes memory arbData = "";
+    function closePosition(bytes32 loanId,uint amount, bool iscollateral, bytes memory arbData)  public{
         IBZx(getBZXRouter()).closeWithSwap(loanId, address(this), amount, iscollateral, arbData);
     }
     function withdrawERC20(address tokenAddress,uint amount) onlyOwner() public{
