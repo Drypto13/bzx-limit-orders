bae98c7916a35c54c933d15d8bf8feb3bec1e601
bZxStats <51417606+bZxStats@users.noreply.github.com>
2021-08-31 10:11:23 -0700

    Delete SmartWalletStorage.sol

--- contracts/SmartWalletStorage.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.8.4;
-
-contract SmartWalletStorage{
-    address internal owner;
-    address internal factoryContract;
-    address internal constant BNBAddress = 0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270;
-
-}
