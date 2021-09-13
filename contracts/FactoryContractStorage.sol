c171fb8332e74d7ab82ac4cbcb7fce932916d3fa
Drypto <51417606+DryptoBZX@users.noreply.github.com>
2021-09-12 18:29:59 -0700

    Revising Names

--- contracts/FactoryContractStorage.sol
+++ contracts/FactoryContractStorage.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.8.4;
 
-contract FactoryContractStorage{
+contract OrderBookStorage{
     address internal bZxRouterAddress = address(0);
     address internal walletGen;
     address internal constant BNBAddress = 0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270;
