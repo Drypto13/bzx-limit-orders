609841bf3333bf88b0e2549ef2a1adf5c8139e2d
Drypto <51417606+DryptoBZX@users.noreply.github.com>
2021-09-12 22:32:24 -0700

    Add files via upload

--- scripts/init_deploy.py
+++ scripts/init_deploy.py
@@ -2,25 +2,25 @@ from brownie import *
 
 def main():
     accounts.load('main3')
-    deploy_base_contracts() 
-    #print(factory.address)
+    ob = deploy_base_contracts() 
+    print(ob.address)
 def deploy_base_contracts():
-    factoryContract = FactoryContract.deploy({'from':accounts[0]}) #deploy factory
-    factoryContractData = FactoryContractData.deploy({'from':accounts[0]})
-    proxyFactory = FactoryContractProxy.deploy("0xfe4F0eb0A1Ad109185c9AaDE64C48ff8e928e54B",{'from':accounts[0]})
+    orderBook = OrderBook.deploy({'from':accounts[0]}) #deploy factory
+    orderBookData = OrderBookData.deploy({'from':accounts[0]})
+    proxyOB = OrderBookProxy.deploy("0xfe4F0eb0A1Ad109185c9AaDE64C48ff8e928e54B",{'from':accounts[0]})
     sigs = []
     targets = []
-    for x in factoryContract.signatures:
-        sigs.append(factoryContract.signatures[x])
-        targets.append(factoryContract.address)
-    for x in factoryContractData.signatures:
-        sigs.append(factoryContractData.signatures[x])
-        targets.append(factoryContractData.address)
+    for x in orderBook.signatures:
+        sigs.append(orderBook.signatures[x])
+        targets.append(orderBook.address)
+    for x in orderBookData.signatures:
+        sigs.append(orderBookData.signatures[x])
+        targets.append(orderBookData.address)
     print(sigs)
     print(targets)
-    proxyFactory.setTargets(sigs,targets,{'from':accounts[0]})
-    KeeperManagement.deploy(proxyFactory.address,{'from':accounts[0]})
-    return proxyFactory
+    proxyOB.setTargets(sigs,targets,{'from':accounts[0]})
+    KeeperManagement.deploy(proxyOB.address,{'from':accounts[0]})
+    return proxyOB
 
     
     
