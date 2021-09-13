b3937e2e49d527170c14914f9ff7a9386cd8c91c
Drypto <51417606+DryptoBZX@users.noreply.github.com>
2021-09-12 17:20:27 -0700

    Updated Contract Deployment

--- scripts/init_deploy.py
+++ scripts/init_deploy.py
@@ -2,19 +2,25 @@ from brownie import *
 
 def main():
     accounts.load('main3')
-    factory = deploy_base_contracts() 
-    print(factory.address)
+    deploy_base_contracts() 
+    #print(factory.address)
 def deploy_base_contracts():
-    factoryContract = walletFactor.deploy({'from':accounts[0]}) #deploy factory
-    smartW = SmartWallet.deploy({'from':accounts[0]})
+    factoryContract = FactoryContract.deploy({'from':accounts[0]}) #deploy factory
+    factoryContractData = FactoryContractData.deploy({'from':accounts[0]})
     proxyFactory = FactoryContractProxy.deploy("0xfe4F0eb0A1Ad109185c9AaDE64C48ff8e928e54B",{'from':accounts[0]})
-    proxyFactory.setImpl(factoryContract.address,{'from':accounts[0]})
-    walletCreation = walletCreator.deploy({'from':accounts[0]}) #deploy wallet generator
-    walletCreation.setFact(proxyFactory.address,{'from':accounts[0]}) #assign the factory address to the generator
-    proxyContract = Contract.from_abi("walletFactor",proxyFactory.address,walletFactor.abi)
-    proxyContract.setWalletLogic(smartW.address,{'from':accounts[0]})
-    proxyContract.setGenerator(walletCreation.address,{'from':accounts[0]}) #assign generatory address to factory
-    return proxyContract
+    sigs = []
+    targets = []
+    for x in factoryContract.signatures:
+        sigs.append(factoryContract.signatures[x])
+        targets.append(factoryContract.address)
+    for x in factoryContractData.signatures:
+        sigs.append(factoryContractData.signatures[x])
+        targets.append(factoryContractData.address)
+    print(sigs)
+    print(targets)
+    proxyFactory.setTargets(sigs,targets,{'from':accounts[0]})
+    KeeperManagement.deploy(proxyFactory.address,{'from':accounts[0]})
+    return proxyFactory
 
     
     
